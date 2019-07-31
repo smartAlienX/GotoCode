b73891308dc09f473d231ab5074cf33a695b8a7f
liucanrong <1373507674@qq.com>
2019-07-31 22:17:36 +0800

    adb

--- src/com/liucr/gotocode/ClickEvent.java
+++ src/com/liucr/gotocode/ClickEvent.java
@@ -10,14 +10,34 @@ public class ClickEvent {
     public List<StackTraceElement> stackTraceElements = new ArrayList<StackTraceElement>();
 
     public static ClickEvent parse(String s) {
-        if (s == null
+        ClickEvent clickEvent = new ClickEvent();
+        if (s != null
                 && s.contains(CLICK_EVENT)) {
             int start = s.indexOf("[");
             int end = s.lastIndexOf("]");
             String infoString = s.substring(start, end);
             String[] infoList = infoString.split(",");
+            for (String info : infoList) {
+                StackTraceElement stackTraceElement = parseToStackTraceElement(info);
+                if (stackTraceElement != null) {
+                    clickEvent.stackTraceElements.add(stackTraceElement);
+                }
+            }
         }
+        return clickEvent;
     }
 
+    public static StackTraceElement parseToStackTraceElement(String s) {
+        if (s != null) {
+            s = s.trim();
+            String[] strings = s.split("\\|");
+            if (strings.length == 4) {
+                return new StackTraceElement(
+                        strings[0], strings[2], strings[1], Integer.parseInt(strings[3])
+                );
+            }
+        }
+        return null;
+    }
 
 }
