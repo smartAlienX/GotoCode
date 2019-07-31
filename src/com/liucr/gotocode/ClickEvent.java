d199232f55ccf6653320c3135b1518cfe7e111d4
liucr <liucanrong@banggood.com>
2019-07-31 19:05:26 +0800

    no message

--- src/com/liucr/gotocode/ClickEvent.java
+++ src/com/liucr/gotocode/ClickEvent.java
@@ -1,8 +1,23 @@
 package com.liucr.gotocode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClickEvent {
 
+    public final static String CLICK_EVENT = "ClickEvent";
 
+    public List<StackTraceElement> stackTraceElements = new ArrayList<StackTraceElement>();
+
+    public static ClickEvent parse(String s) {
+        if (s == null
+                && s.contains(CLICK_EVENT)) {
+            int start = s.indexOf("[");
+            int end = s.lastIndexOf("]");
+            String infoString = s.substring(start, end);
+            String[] infoList = infoString.split(",");
+        }
+    }
 
 
 }
