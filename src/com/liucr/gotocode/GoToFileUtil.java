300a540279f9234b8f515caab4410cd0db14d18c
liucr <liucanrong@banggood.com>
2019-09-27 16:38:19 +0800

    no message

--- src/com/liucr/gotocode/GoToFileUtil.java
+++ src/com/liucr/gotocode/GoToFileUtil.java
@@ -1,13 +1,14 @@
 package com.liucr.gotocode;
 
+import com.intellij.lang.jvm.JvmMethod;
+import com.intellij.lang.jvm.JvmParameter;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.JavaPsiFacade;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiFile;
+import com.intellij.psi.*;
 import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.PsiShortNamesCache;
 
 public class GoToFileUtil {
 
@@ -27,6 +28,31 @@ public class GoToFileUtil {
                         containingFile = psiClass.getContainingFile();
                         VirtualFile virtualFile = containingFile.getVirtualFile();
                         OpenFileDescriptor openFileDescriptor = new OpenFileDescriptor(project, virtualFile, traceElement.getLineNumber(), 10);
+                        openFileDescriptor.navigate(true);
+                    }
+                }));
+    }
+
+    public static void openFile(Project project, String className) {
+        ApplicationManager.getApplication().invokeLater(() ->
+                ApplicationManager.getApplication().runReadAction(() -> {
+                    PsiClass psiClass = JavaPsiFacade.getInstance(project)
+                            .findClass(className, GlobalSearchScope.allScope(project));
+                    PsiFile containingFile = null;
+                    if (psiClass != null) {
+                        containingFile = psiClass.getContainingFile();
+                    } else {
+                        PsiFile[] filesByName = PsiShortNamesCache.getInstance(project).getFilesByName(className + ".java");
+                        if (filesByName.length == 0) {
+                            filesByName = PsiShortNamesCache.getInstance(project).getFilesByName(className + ".kt");
+                        }
+                        if (filesByName.length > 0) {
+                            containingFile = filesByName[0];
+                        }
+                    }
+                    if (containingFile != null) {
+                        VirtualFile virtualFile = containingFile.getVirtualFile();
+                        OpenFileDescriptor openFileDescriptor = new OpenFileDescriptor(project, virtualFile);
                         openFileDescriptor.navigate(true);
                     }
                 }));
--- src/com/liucr/gotocode/toolwindow/GoToActivityTab.java
+++ src/com/liucr/gotocode/toolwindow/GoToActivityTab.java
@@ -1,6 +1,7 @@
 package com.liucr.gotocode.toolwindow;
 
 import com.intellij.openapi.project.Project;
+import com.liucr.gotocode.GoToFileUtil;
 import com.liucr.gotocode.tool.ActivityInformationTool;
 import com.liucr.gotocode.tool.ActivityInformationTool.ActivityInformation;
 
@@ -36,17 +37,26 @@ public class GoToActivityTab extends BaseTab {
             }
         });
 
-        currentActivity.addMouseListener(new MouseAdapter() {
+        currentActivity.addMouseListener(new FocusMouseAdapter(currentActivity) {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
+                if (activityInformation != null) {
+                    String name = activityInformation.getName();
+                    GoToFileUtil.openFile(project, name);
+                }
             }
         });
 
-        currentFragment.addMouseListener(new MouseAdapter() {
+        currentFragment.addMouseListener(new FocusMouseAdapter(currentFragment) {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
+                if (activityInformation != null
+                        && activityInformation.getCurFragmentInformation() != null) {
+                    String name = activityInformation.getCurFragmentInformation().getSimpleName();
+                    GoToFileUtil.openFile(project, name);
+                }
             }
         });
     }
@@ -59,20 +69,21 @@ public class GoToActivityTab extends BaseTab {
 
         if (activityInformation.getCurFragmentInformation() != null) {
             currentFragment.setText(activityInformation.getCurFragmentInformation().getSimpleName());
-        }else {
+        } else {
             currentFragment.setText(null);
         }
 
-        if(fragmentListPanel.getComponentCount()!=0){
+        if (fragmentListPanel.getComponentCount() != 0) {
             fragmentListPanel.removeAll();
         }
         List<ActivityInformationTool.FragmentInformation> informationList = activityInformation.getInformationList();
         for (ActivityInformationTool.FragmentInformation fragmentInformation : informationList) {
             JLabel jLabel = new JLabel(fragmentInformation.getSimpleName());
-            jLabel.addMouseListener(new MouseAdapter() {
+            jLabel.addMouseListener(new FocusMouseAdapter(jLabel) {
                 @Override
                 public void mouseClicked(MouseEvent e) {
                     super.mouseClicked(e);
+                    GoToFileUtil.openFile(project, fragmentInformation.getSimpleName());
                 }
             });
             fragmentListPanel.add(jLabel);
@@ -82,5 +93,28 @@ public class GoToActivityTab extends BaseTab {
 
     public Component getComponent() {
         return content;
+    }
+
+    public class FocusMouseAdapter extends MouseAdapter{
+
+        private JLabel jLabel;
+        private Color color;
+
+        public FocusMouseAdapter(JLabel jPanel) {
+            this.jLabel = jPanel;
+            color = jPanel.getForeground();
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            super.mouseEntered(e);
+            jLabel.setForeground(Color.decode("#5188BA"));
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            super.mouseExited(e);
+            jLabel.setForeground(color);
+        }
     }
 }
